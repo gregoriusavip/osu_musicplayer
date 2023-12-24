d46ecbca8a04550d91c183fee81fce54f15a3583
Gregorius Avip <gregorius.avip@gmail.com>
2023-12-23 18:20:33 -0800

    Update logging

--- folder/main.py
+++ folder/main.py
@@ -2,19 +2,25 @@ import settings
 import logging
 from path_scanner import path_scanner
 from create_db import create_db
+import song_parser
 
-settings.init()
-
-if not settings.osu_folder:
-    print("osu songs folder cannot be located. please select the correct directory")
-    path = "some_path"
-    settings.osu_folder = path_scanner(path)
+logging.basicConfig(format='%(asctime)s %(message)s', filename="debug.log", level=logging.DEBUG)
 
 def main():
-    logging.basicConfig(format='%(asctime)s %(message)s', filename="debug.log", level=logging.DEBUG)
-    logging.info("Starting application...")
+    settings.init()
+    if not settings.osu_folder:
+        logging.info("osu songs folder cannot be located.")
+        path = "some_path"
+        settings.osu_folder = path_scanner(path)
+        
     create_db()
-    logging.info("Quitting appliaction...")
+
+    from pathlib import Path
+    import os
+    songs_directory = os.path.join(Path.home(), 'Appdata', 'Local', 'osu!', "Songs", "210 Taiko no Tatsujin - Saitama2000")
+    song_parser.extract_beatmap_id(os.path.basename(songs_directory))
 
 if __name__ == '__main__':
+    logging.info("Starting application...")
     main()
+    logging.info("Quitting appliaction...\n")
