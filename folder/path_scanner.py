fc510389bdef001751bc51f098b531bbdf35fd47
Gregorius Avip <gregorius.avip@gmail.com>
2023-12-23 18:19:33 -0800

    Added DOCSTRING to functions

--- folder/path_scanner.py
+++ folder/path_scanner.py
@@ -1,26 +1,30 @@
 from pathlib import Path
 import os
 
-"""
-scans the user from home directory to locate osu folder
-default scanner to locate the osu folder. 
-run path_scanner instead if default location is not found
-
-@return: path to the songs folder if exist, None otherwise
-"""
 def default_scanner():
+    """Scans the user from home directory to locate osu folder.
+
+    NOTE: Run `path_scanner` instead if default location is not found.
+
+    Returns: 
+    `str`: path to the songs folder if exist, `None` otherwise
+    """
     songs_directory = os.path.join(Path.home(), 'Appdata', 'Local', 'osu!', "Songs")
     if not os.path.exists(songs_directory):
         return None
     return songs_directory
 
-"""
-function to check if a given folder directory contains "Songs" folder
-NOTE: does not check if it is a valid osu folder or if the songs folder is invalid
+def path_scanner(osu_path: str):
+    """function to check if a given folder directory contains "Songs" folder
 
-@return: path to the songs folder if exist, None otherwise
-"""
-def path_scanner(osu_path):
+    NOTE: does not check if it is a valid osu folder or if the songs folder is invalid
+
+    Parameters:
+    `osu_path` (str): path to the folder containing `Songs` folder
+
+    Returns: 
+    `str`: path to the songs folder if exist, `None` otherwise
+    """
     songs_directory = os.path.join(osu_path, "Songs")
     if not os.path.exists(songs_directory):
         return None
