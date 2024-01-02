cc799b39d4061a0d89f4873ecf39ea8c902db01e
Gregorius Avip <gregorius.avip@gmail.com>
2024-01-02 01:51:06 -0800

    Create scanner.py

---
+++ src/scan_folder/scanner.py
@@ -0,0 +1,17 @@
+import os
+import logging
+import fnmatch
+import glob
+from scan_folder.song_parser import song_parser
+
+def scanner(songs_directory):
+    logging.info("SCANNING START")
+    if os.path.exists(songs_directory):
+        for root, _, _ in os.walk(os.path.abspath(songs_directory)):
+            files = glob.glob(os.path.join(root,"*.osu"))
+            for file in files:
+                if fnmatch.fnmatch(file, "*.osu"):
+                    with open(os.path.join(root, file), "r", encoding='utf-8-sig') as osu_file:
+                        logging.info("Reading " + file)
+                        song_parser(osu_file, os.path.basename(root))
+    logging.info("SCANNING SUCCESS")
