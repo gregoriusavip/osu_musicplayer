a19deb3cd51d58870397f1c649bce03ab1ad7502
Gregorius Avip <gregorius.avip@gmail.com>
2024-01-10 18:22:41 -0800

    Update sql_error.py
    add comments

--- src/db/sql_error.py
+++ src/db/sql_error.py
@@ -1,7 +1,15 @@
 import logging
 
-def sql_error_handler(e, message):
-    reason = "Task " + message + " could not be completed."
+def sql_error_handler(e, task):
+    """
+    Helper function to handle when an error occured during sqlite operation.
+
+    :param1 `e`: the exception that was caught on the catch block
+    :param2 `message`: the task operation that was attempted
+
+    :return: no return value
+    """
+    reason = "Task: " + task + " could not be completed."
     logging.critical(reason)
     logging.error('Sql error: %s' % (' '.join(e.args)))
     logging.error("Exception class is: ", e.__class__)
