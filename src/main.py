675f8aaafea83e15e5c1ad37a02abff652f148ad
Gregorius Avip <gregorius.avip@gmail.com>
2024-01-21 00:05:20 -0800

    Update main.py
    some clean-up/added a timeit for testing query time

--- src/main.py
+++ src/main.py
@@ -1,10 +1,19 @@
+# timer/profiler for debugging
+import cProfile
+import timeit
+from functools import partial
+
+# settings and logging
 import settings
 import logging
+
+# database/scanner functions
 from path_scanner import database_exists
 from db.create_db import create_db
+from db.db_operation import query_beatmap, default_select
 from scan_folder.scanner import scanner
-from db.db_operation import query_beatmap
-import cProfile
+
+# music player functionality
 from music_player.controller import test
 
 def main():
@@ -16,8 +25,10 @@ def main():
     if not database_exists():
         create_db()
         songs_directory = settings.OSU_FOLDER
-        scanner(songs_directory)
-    query_beatmap("Nakuru")
+        scanner(songs_directory)   # TODO: handle error
+    partial_function = partial(query_beatmap, user_query="nakuru", sort_by="Title")
+    execution_time = timeit.timeit(partial_function, number=1)
+    print(f"Execution time: {execution_time} seconds")
     test()
 
 if __name__ == '__main__':
