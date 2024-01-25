b01322c95bad3e33c310d939335ecd1ae49035f4
Gregorius Avip <gregorius.avip@gmail.com>
2024-01-24 18:31:52 -0800

    Update settings.py
    add a backup for logging

--- src/settings.py
+++ src/settings.py
@@ -26,8 +26,8 @@ def init():
     # LOGGING SETTINGS
     log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(funcName)s(%(lineno)d) %(message)s')
     logFile = os.path.join(os.getcwd(), 'log')
-    handler = RotatingFileHandler(logFile, mode='w', maxBytes=50*1024, 
-                                  encoding='utf-8', delay=0)
+    handler = RotatingFileHandler(logFile, mode='w', maxBytes=500*1024, 
+                                  encoding='utf-8', delay=0, backupCount=1)
     handler.setFormatter(log_formatter)
     handler.setLevel(logging.INFO)
     app_log = logging.getLogger('root')
